fa5ed6f3c47841116b1231cc0389fd3e83676f93
Laure Descouts <lauredscts@gmail.com>
2023-05-25 17:06:04 +0200

    first part watch list

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
   get '/lists', to: 'lists#index'
   get 'lists/new', to: 'lists#new'
-  get 'lists/:id', to: 'lists#show'
+  get 'lists/:id', to: 'lists#show', as: :list
   post 'lists', to: 'lists#create'
   get 'lists/:id/edit', to: 'lists#edit'
   patch 'lists/:id', to: 'lists#update'
