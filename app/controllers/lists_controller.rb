b980f860af9d753e849658b644d19fbfecb6cdb6
Laure Descouts <lauredscts@gmail.com>
2023-05-26 17:05:49 +0200

    watch list part 2

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,11 +1,12 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:show, :destroy]
 
   def index
     @lists = List.all
   end
 
   def show
-    @list = List.find(params[:id])
+    @bookmark = Bookmark.new
   end
 
   def new
@@ -22,12 +23,15 @@ class ListsController < ApplicationController
   end
 
   def destroy
-    @list = List.find(params[:id])
     @list.destroy
-    redirect_to list_path, status: :see_other
+    redirect_to lists_path
   end
 
   private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
 
   def list_params
     params.require(:list).permit(:name)
--- config/routes.rb
+++ config/routes.rb
@@ -2,21 +2,11 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
-  get '/lists', to: 'lists#index'
-  get 'lists/new', to: 'lists#new'
-  get 'lists/:id', to: 'lists#show', as: :list
-  post 'lists', to: 'lists#create'
-  get 'lists/:id/edit', to: 'lists#edit'
-  patch 'lists/:id', to: 'lists#update'
-  delete 'lists/:id', to: 'lists#destroy'
-
-  # resources :bookmarks, only: :destroy
-  # resources :reviews, only: :destroy
+  root to: 'lists#index'
+  resources :lists, except: [:edit, :update] do
+    resources :bookmarks, only: [:new, :create]
+    resources :reviews, only: :create
+  end
+  resources :bookmarks, only: :destroy
+  resources :reviews, only: :destroy
 end
-# FINISHING THE ROUTES
-
-# resources :lists do
-#   resources :lists, only: [:new, :create]
-# end
-# resources :reviews, only: [:destroy]
-# end
